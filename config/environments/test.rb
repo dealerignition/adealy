a196c2ef89f7e442b2c798fc331b0234b30e7aa4
Justin Wilson <justin@jwils.co>
2013-02-19 14:41:14 -0500

    new rails 3 app for use as static site

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,35 +1,37 @@
-# Settings specified here will take precedence over those in config/environment.rb
+Adealy::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
-# The test environment is used exclusively to run your application's
-# test suite.  You never need to work with it otherwise.  Remember that
-# your test database is "scratch space" for the test suite and is wiped
-# and recreated between test runs.  Don't rely on the data there!
-config.cache_classes = true
+  # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is "scratch space" for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
 
-# ensure test extensions are loaded
-# test_extension_dir = File.join(File.expand_path(RADIANT_ROOT), 'test', 'fixtures', 'extensions')
-# config.extension_paths.unshift test_extension_dir
-# config.extension_paths.uniq!
-# if !config.extensions.include?(:all)
-#   config.extensions.concat(Dir["#{test_extension_dir}/*"].sort.map {|x| File.basename(x).sub(/^\d+_/,'')})
-#   config.extensions.uniq!
-# end
+  # Configure static asset server for tests with Cache-Control for performance
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=3600"
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils    = true
+  # Log error messages when you accidentally call methods on nil
+  config.whiny_nils = true
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
-# Disable request forgery protection in test environment
-config.action_controller.allow_forgery_protection    = false
+  # Raise exceptions instead of rendering exception templates
+  config.action_dispatch.show_exceptions = false
 
-# Tell ActionMailer not to deliver emails to the real world.
-# The :test delivery method accumulates sent emails in the
-# ActionMailer::Base.deliveries array.
-config.action_mailer.delivery_method = :test
+  # Disable request forgery protection in test environment
+  config.action_controller.allow_forgery_protection    = false
 
-config.gem "rspec-rails", :version => "~>1.2.6", :lib => false
-config.gem "webrat", :version => "~>0.4.4", :lib => false
-config.gem "cucumber", :version => "~>0.3.9", :lib => false
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Print deprecation notices to the stderr
+  config.active_support.deprecation = :stderr
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adealy::Application.config.secret_token = 'b4e153cf73a3860348e04172cf46f17f9ca6b823db31e1bf0595982f64ee6475cdfe89ac0c25b9379eb2d9ef4dd8c0aa66d9e6d6c886697266b68102abc0f902'
